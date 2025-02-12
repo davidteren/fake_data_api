3dea94ccb496eab4e60c7ec5ba801b2281778056
davidteren <drteren@gmail.com>
2025-02-12 19:11:16 +0200

    Simplify deployment configuration

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# frozen_string_literal: true
-
 require 'sinatra'
 require 'sinatra/json'
 require 'faker'
--- config/puma.rb
+++ config/puma.rb
@@ -5,14 +5,11 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# Specifies the `port` that Puma will listen on to receive requests
 port ENV['PORT'] || 3000
 
-# Specifies the `environment` that Puma will run in.
+# Specifies the `environment` that Puma will run in
 environment ENV['RACK_ENV'] || 'development'
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
-
-# Redirect STDOUT and STDERR to files specified
-stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true
+# Bind to all network interfaces
+bind 'tcp://0.0.0.0:' + (ENV['PORT'] || '3000')
