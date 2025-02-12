da57ce86c2a7bb7638da969bc84a99d082a3bd8b
davidteren <drteren@gmail.com>
2025-02-12 18:22:16 +0200

    Initial commit: Create Sinatra API with fake data generation

---
+++ app.rb
@@ -0,0 +1,59 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'faker'
+
+# Enable CORS
+before do
+  content_type :json
+  headers 'Access-Control-Allow-Origin' => '*',
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+end
+
+# Landing page
+get '/' do
+  content_type :html
+  erb :index
+end
+
+# API Documentation page
+get '/docs' do
+  content_type :html
+  erb :docs
+end
+
+# API Endpoints
+# Generate a single person
+get '/api/person' do
+  json generate_person
+end
+
+# Generate multiple people
+get '/api/people/:count' do |count|
+  count = count.to_i.clamp(1, 10)
+  people = count.times.map { generate_person }
+  json people
+end
+
+private
+
+def generate_person
+  {
+    id: Faker::Number.unique.number(digits: 10),
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone,
+    address: {
+      street: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zip: Faker::Address.zip_code,
+      country: Faker::Address.country
+    },
+    company: {
+      name: Faker::Company.name,
+      position: Faker::Job.title
+    },
+    avatar: Faker::Avatar.image
+  }
+end
